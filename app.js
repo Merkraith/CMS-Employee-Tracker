d69ed30d52807125f68dd5197ba7f8b91caafcf1
Mark Heil <mark.heil1122@gmail.com>
2020-08-11 01:42:50 -0700

    first commit, setting up app.js and creating server

---
+++ app.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "process.env.DB_password",
+    database: "employee_tracker_DB"
+  });
+  
+  // connect to the mysql server and sql database
+  connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
