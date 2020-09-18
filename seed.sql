df5d0aed0f60980e58c5d814cbeca8f333b261aa
Mark Heil <mark.heil1122@gmail.com>
2020-09-18 01:05:57 -0700

    update seed.sql

--- seed.sql
+++ seed.sql
@@ -1,13 +1,43 @@
-use employee_tracker_DB
+DROP DATABASE IF EXISTS employees_DB;
+CREATE database employees_DB;
 
-INSERT INTO employee (employee_id, first_name, last_name, role_id)
-VALUES (423403, 'Missy', 'Becker', 10), (134034, 'Mathew', 'Lebowski', 10), (234031, 'Julie', 'Barwick', 10), (034366, 'Alicia', 'Gardner', 10)
+USE employees_DB;
 
-INSERT INTO employee (employee_id, first_name, last_name, role_id)
-VALUES (543224, 'Bob', 'Harrits', 20), (134444, 'Jeff', 'Pond', 20), (238042, 'Evan', 'Miles', 20), (253523, 'Mathew', 'Honore', 20)
+CREATE TABLE department (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department VARCHAR(30) NULL,
+  PRIMARY KEY (department_id)
+);
 
-INSERT INTO employee (employee_id, first_name, last_name, role_id)
-VALUES (523532, 'Dade', 'Murphy', 30), (287423, 'Eli', 'Stone', 30), (334455, 'Aldus', 'Snow', 30), (456125, 'Jeffery', 'Brand', 30)
+CREATE TABLE roles (
+  roles_id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  department_id INT,
+  PRIMARY KEY (roles_id)
+);
 
-INSERT INTO employee (employee_id, first_name, last_name, role_id)
-VALUES (332154, 'Billy', 'Madison', 40), (423464, 'Happy', 'Gilmore', 40), (477423, 'Donald', 'Berger', 40), (209572, 'Bobby', 'Boucher', 40)
+CREATE TABLE employee (
+  employee_id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  roles_id INT NULL,
+  PRIMARY KEY (employee_id)  
+);
+
+SELECT * FROM department;
+SELECT * FROM roles;
+SELECT * FROM employee;
+
+INSERT INTO department (department_id, department)
+VALUES (1, "Engineering"), (2, "Accounting"), (3, "Sales");
+
+INSERT INTO roles (roles_id, title, department_id)
+VALUES (1, "Software Engineer", 1), (2, "Full Stack Developer", 1), 
+ (3, "Accountant", 2), (4, "Accountant Supervisor", 2),
+(5, "Sales Rep", 3), (6, "Sales Supervisor", 3);
+
+INSERT INTO employee (employee_id, first_name, last_name, roles_id)
+VALUES (1, "Scarlett", "Johanson", 1), (2, "Paul", "Rudd", 2), (3, "Samuel", "Jackson", 3), (4, "Vin", "Diesel", 1), 
+(5, "Chris", "Pratt", 1), (6, "Robert", "Downey Jr", 2), (7, "Bradley", "Cooper", 3), (8, "Jeff", "Goldblum", 2), 
+(9, "Chris", "Hemsworth", 1), (10, "Jon", "Favreu", 2), (11, "Chris", "Evans", 3), (12, "Jeff", "Bridges", 3),
+
