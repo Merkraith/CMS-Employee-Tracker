301d3445e11a7b2db41afcc07e48083601daff3f
Mark Heil <mark.heil1122@gmail.com>
2020-09-18 00:26:18 -0700

    redid seed.sql

---
+++ seed.sql
@@ -0,0 +1,13 @@
+use employee_tracker_DB
+
+INSERT INTO employee (employee_id, first_name, last_name, role_id)
+VALUES (423403, 'Missy', 'Becker', 10), (134034, 'Mathew', 'Lebowski', 10), (234031, 'Julie', 'Barwick', 10), (034366, 'Alicia', 'Gardner', 10)
+
+INSERT INTO employee (employee_id, first_name, last_name, role_id)
+VALUES (543224, 'Bob', 'Harrits', 20), (134444, 'Jeff', 'Pond', 20), (238042, 'Evan', 'Miles', 20), (253523, 'Mathew', 'Honore', 20)
+
+INSERT INTO employee (employee_id, first_name, last_name, role_id)
+VALUES (523532, 'Dade', 'Murphy', 30), (287423, 'Eli', 'Stone', 30), (334455, 'Aldus', 'Snow', 30), (456125, 'Jeffery', 'Brand', 30)
+
+INSERT INTO employee (employee_id, first_name, last_name, role_id)
+VALUES (332154, 'Billy', 'Madison', 40), (423464, 'Happy', 'Gilmore', 40), (477423, 'Donald', 'Berger', 40), (209572, 'Bobby', 'Boucher', 40)
